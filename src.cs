4c3fbdaa24b449759aa57998f1210db0ad8aefaa
Muhanad <45791558+ksosos@users.noreply.github.com>
2022-04-14 14:41:25 +0200

    Update src.cs

--- src.cs
+++ src.cs
@@ -21,15 +21,10 @@ namespace tic_tac_toe
         void end()
         {
             
-            button1.Enabled = false;
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-            button5.Enabled = false;
-            button6.Enabled = false;
-            button7.Enabled = false;
-            button8.Enabled = false;
-            button9.Enabled = false;
+            foreach(var button in this.Controls.OfType<Button>())
+        {
+            button.Enabled = false;
+        }
         }
         private void button9_Click_1(object sender, EventArgs e)
         {
